e582a28be56a43013ecc3e26df260f4c6cb76077
twnbay78 <leoscarano6@gmail.com>
2023-06-01 04:00:39 -0400

    Enabling transparent mode in gruvbox

---
+++ plugins/configs/gruvbox.lua
@@ -0,0 +1,4 @@
+return function(_, opts)
+  local gruvbox = require "gruvbox"
+  gruvbox.setup(opts)
+end
---
+++ plugins/gruvbox.lua
@@ -0,0 +1,9 @@
+return {
+  "ellisonleao/gruvbox.nvim", 
+  priority = 1000,
+  opts = function()
+    return {
+      transparent_mode = true
+    }
+  end,
+}

39141f7131cd5bb41694f8d3f7e19360d79fe85f
twnbay78 <leoscarano6@gmail.com>
2023-06-04 10:54:54 -0400

    Updating plugin conf

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -1,5 +1,7 @@
 return {
-  --"github/copilot.vim", -> this freezes on :Copilot auth / :Copilot setup for some reason, trying lua port
   "Exafunction/codeium.vim",
+  config = function()
+    vim.keymap.set('i', '<C-;>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+  end,
   lazy = false,
 }
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,6 +1,8 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
+    local null_ls = require("null-ls") 
+
     -- config variable is the default configuration table for the setup function call
     -- local null_ls = require "null-ls"
 
@@ -11,6 +13,10 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      --null_ls.builtins.code_actions.eslint,
+      --null_ls.builtins.formatting.eslint,
+      --null_ls.builtins.diagnostics.eslint
+
     }
     return config -- return final config table
   end,
